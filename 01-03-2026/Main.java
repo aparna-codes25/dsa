52ae95e13d384172a429b496aa4959078b5565e5
aparna-codes25 <codes.aparna@gmail.com>
2026-03-01 17:44:43 +0530

    Added POTD Solution of March 1, 2026 - Move All Zeroes to End

--- 01-03-2026/Main.java
+++ 01-03-2026/Main.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // You can add test cases here to run your code
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        MoveAllZeroestoEnd sol = new MoveAllZeroestoEnd();  
+        sol.pushZerosToEnd(arr);
+       for(int i = 0; i<n; i++){
+            System.out.print(arr[i] + " ");
+       }
+       sc.close();
     }
 }
