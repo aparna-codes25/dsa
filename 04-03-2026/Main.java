1db5636b1b17ba4952309b7589ea4610d8481a90
aparna-codes25 <codes.aparna@gmail.com>
2026-03-04 09:55:07 +0530

    Added POTD Solution of March 4, 2026 - Max Xor Subarray of size K

---
+++ 04-03-2026/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0;i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        MaxXorSubarrayOfsizeK sol = new MaxXorSubarrayOfsizeK();
+        int result = sol.maxSubarrayXOR(arr, k);
+        System.out.println(result);
+        sc.close();
+    }
+    
+}
