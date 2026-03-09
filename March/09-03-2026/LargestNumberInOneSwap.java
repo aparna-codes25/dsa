91a9d639b44aa4b029b912e15cc3109fd6bb710c
aparna-codes25 <codes.aparna@gmail.com>
2026-03-09 13:09:09 +0530

    Added POTD Solution of March 9, 2026

---
+++ March/09-03-2026/LargestNumberInOneSwap.java
@@ -0,0 +1,50 @@
+
+public class LargestNumberInOneSwap {
+    public String largestSwap(String s) {
+        // code here
+        
+        // Idea is smallest leftmost digit has a larger place value
+        // than the rightmost largest digit.
+        // We find the rightmost largest digit and leftmost smallest digit (which has highest possible 
+        // place value) and swap both
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        
+        int maxInd = -1;
+        int maxDigit = -1;
+        
+        int l = -1;
+        int r = -1;
+        
+        char temp;
+        
+        for(int i = n-1;i>=0;i--){
+            int digit = arr[i] - '0';
+            if(digit > maxDigit){
+                maxDigit = digit;
+                maxInd = i;
+            }   
+            
+            // Finding a small digit from right to left that can be
+            // swapped with the max Digit
+            // l -> index of smallest digit in left
+            // r -> index of largest digit in the right
+            
+            
+            else if(digit < maxDigit){
+                l = i;
+                r = maxInd;
+            }
+        }
+        
+        // No swapping is required
+        // maxDigit and smallest digit with largest place value is same
+        if(l == -1) return s;
+        
+        temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+        
+        return new String(arr);
+    }
+}
